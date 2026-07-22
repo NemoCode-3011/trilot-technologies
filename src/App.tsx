19a2c6cf48a3a21a7a910bcc61724c21a8a5f125
Adeniyi Temilade Vivian <adeniyitemilade192@gmail.com>
2026-07-22 13:03:41 +0100

    feat: added in scrolltotop

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Work from "./pages/Work"
-import DePrime from "./pages/works/DePrime"
-import Orita from "./pages/works/Orita"
-import Arcadia from "./pages/works/Arcadia"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import LoadingScreen from "./components/LoadingScreen"
-import ContactModal from "./components/ContactModal"
-import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import DePrime from "./pages/works/DePrime";
+import Orita from "./pages/works/Orita";
+import Arcadia from "./pages/works/Arcadia";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LoadingScreen from "./components/LoadingScreen";
+import ContactModal from "./components/ContactModal";
+import Scrolltotop from "./components/ScrollToTop";
+import { useState } from "react";
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <BrowserRouter>
       <LoadingScreen />
+      <Scrolltotop />
       <ContactModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
       <Navbar onContactClick={() => setModalOpen(true)} />
       <Routes>
-        <Route path="/" element={<Home onContactClick={() => setModalOpen(true)} />} />
+        <Route
+          path="/"
+          element={<Home onContactClick={() => setModalOpen(true)} />}
+        />
         <Route path="/work" element={<Work />} />
         <Route path="/work/de-prime" element={<DePrime />} />
         <Route path="/work/orita" element={<Orita />} />
@@ -27,7 +32,7 @@ function App() {
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
